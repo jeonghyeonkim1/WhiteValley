19933ae1d11e24922f35125a71f82d39bca82034
jeonghyeonkim1 <102194788+jeonghyeonkim1@users.noreply.github.com>
2022-03-28 13:48:37 +0900

    jh#0328_2
    1. project 폴더 -> urls.py
    => 전 app에 권한부여

--- Proj_WV/urls.py
+++ Proj_WV/urls.py
@@ -3,5 +3,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('whitevalley/', include('shop.urls'))
+    path('whitevalley/', include('shop.urls')),
+    path('whitevalley/', include('user.urls')),
+    path('whitevalley/', include('order.urls')),
+    path('whitevalley/', include('recommendation.urls')),
+    path('whitevalley/', include('cs.urls')),
 ]
