34d514442677036220783ac08741db3c7a1340a9
jjangseo1 <93521234+seoheejj1@users.noreply.github.com>
2022-03-30 21:55:20 +0900

    sh#0330_1

--- order/urls.py
+++ order/urls.py
@@ -1,5 +1,13 @@
+from unicodedata import name
 from django.urls import path
+from . import views
+
+app_name = "Order"
 
 urlpatterns = [
+    path('order/', views.order, name="order"),
+    path('order/order_des/',views.order_des, name="order_des"),
+    path('order/order_des/custom1/',views.custom1 , name="custom1"),
+    path('order/order_des/custom1/custom2/',views.custom2 , name="custom2"),
     
 ]
