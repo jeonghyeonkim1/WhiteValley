433fed0f8c3033ee2fbe73a810deca4134fbc770
jeonghyeonkim1 <102194788+jeonghyeonkim1@users.noreply.github.com>
2022-03-30 21:55:06 +0900

    jh#0330_2

--- shop/views.py
+++ shop/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 
-
 def home(req):
     context = {
-        'currentPage': 'home'
+        'cookies': req.COOKIES
     }
 
-    return render(req, 'home.html', context)
+    cookie_name = 'visited'
+    cookie_value = True
+
+    context['cookie_name'] = cookie_name
+    context['cookie_value'] = cookie_value
+
+    res = render(req, 'home.html', context)
+
+    res.set_cookie(
+        key=cookie_name,
+        value=cookie_value,
+        expires=360
+    )
+
+    return res
