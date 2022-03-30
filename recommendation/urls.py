6880653b298cc7cc69854ed4e164fd47b53a4c9b
dongwan123 <s97741875@gmail.com>
2022-03-30 21:55:21 +0900

    dw#0330_1

--- recommendation/urls.py
+++ recommendation/urls.py
@@ -1,6 +1,14 @@
 
-from django.urls import path, include
+from django.urls import path
+from . import views
+
+app_name = "Recommendation"
 
 urlpatterns = [
-    
+    path('reviews/',views.reviews, name='reviews'),
+    path('reviews_detail/',views.reviews_detail, name='reviews_detail'),
+    path('tag_reviews/',views.tag_reviews, name='tag_reviews'),
+    path('tag_reviews_detail/',views.tag_reviews_detail, name='tag_reviews_detail'),
+    path('finished/',views.finished, name='finished'),
+    path('finished_detail/',views.finished_detail, name='finished_detail'),
 ]
