9e927bb83239594a11668efe152084592bee84fd
kimdoheee <dohee.kimdh@gmail.com>
2022-03-30 21:55:40 +0900

    dh#0330_1

--- cs/urls.py
+++ cs/urls.py
@@ -4,4 +4,14 @@ from cs import views
 app_name = "Cs"
 
 urlpatterns = [
+    path("write/", views.notice_write, name="notice_write"),
+    path("detail/<int:pk>/", views.notice_detail, name="notice_detail"),
+    path("list/", views.notice_list, name="notice_list"),
+    path('update/<int:pk>/', views.notice_update, name="notice_update"),
+    path('delete/', views.notice_delete, name="notice_delete"),
+    path("event/write/", views.event_write, name="event_write"),
+    path("event/detail/<int:pk>/", views.event_detail, name="event_detail"),
+    path("event/list/", views.event_list, name="event_list"),
+    path('event/update/<int:pk>/', views.event_update, name="event_update"),
+    path('event/delete/', views.event_delete, name="event_delete"),
 ]
--- cs/views.py
+++ cs/views.py
@@ -1,3 +1,70 @@
 from django.shortcuts import render
 
-# Create your views here.
+def notice_write(req):
+    context = {
+        'currentPage': 'c/s'
+    }
+    return render(req, 'notice_write.html', context)
+    
+
+def notice_detail(req, pk):
+    context = {
+        'currentPage': 'c/s'
+    }
+    return render(req, 'notice_detail.html', context)
+
+
+def notice_list(req):
+    context = {
+        'currentPage': 'c/s'
+    }
+    return render(req, 'notice_list.html', context)
+
+
+def notice_update(req, pk):
+    context = {
+        'currentPage': 'c/s'
+    }
+    return render(req, 'notice_update.html', context)
+
+
+def notice_delete(req):
+    context = {
+        'currentPage': 'c/s'
+    }
+    return render(req, 'notice_delete.html', context)
+
+
+def event_write(req):
+    context = {
+        'currentPage': 'c/s'
+    }
+    return render(req, 'event_write.html', context)
+
+
+def event_detail(req):
+    context = {
+        'currentPage': 'c/s'
+    }
+    return render(req, 'event_detail.html', context)
+
+
+def event_list(req):
+    context = {
+        'currentPage': 'c/s'
+    }
+    return render(req, 'event_list.html', context)
+
+
+def event_update(req):
+    context = {
+        'currentPage': 'c/s'
+    }
+    return render(req, 'event_update.html', context)
+
+
+def event_delete(req):
+    context = {
+        'currentPage': 'c/s'
+    }
+    return render(req, 'event_delete.html', context)
