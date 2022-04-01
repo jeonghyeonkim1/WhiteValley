af6daf1ec4807df6b794ad32d60ca4c28e8f11fd
kimdoheee <dohee.kimdh@gmail.com>
2022-04-01 15:20:18 +0900

    dh#0401_2

--- cs/admin.py
+++ cs/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from cs.models import Board
 
 class BoardAdmin(admin.ModelAdmin):
-    list_display = ('id', 'tag', 'title')
+    list_display = ('id', 'tag', 'title',)
 
 admin.site.register(Board, BoardAdmin)
