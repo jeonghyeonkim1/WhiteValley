a08e933342ca8122c04c08ae8b32b33ba38b1913
jjangseo1 <93521234+seoheejj1@users.noreply.github.com>
2022-04-04 09:06:01 +0900

    sh#0404_1

--- order/views.py
+++ order/views.py
@@ -1,6 +1,12 @@
+import base64
+from random import random
+from unittest import result
 from django.shortcuts import render,redirect
 from shop.models import Config
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse 
+import os
 
 # Create your views here.
 def order(request):
@@ -85,31 +91,23 @@ def loading(request):
 def loading2(request):
     return render(request, 'loading2.html')
 
-#    if request.method == "POST":
-#         URL = 'https://kapi.kakao.com/v1/payment/ready'
-#         headers = {
-#             "Authorization": "KakaoAK " + "953b61f21c9f3ddc6d49f10981368940",   # 변경불가
-#             "Content-type": "application/x-www-form-urlencoded;charset=utf-8",  # 변경불가
-#         }
-#         params = {
-#             "cid": "TC0ONETIME",    # 테스트용 코드
-#             "partner_order_id": "1001",     # 주문번호
-#             "partner_user_id": "german",    # 유저 아이디
-#             "item_name": "연어초밥",        # 구매 물품 이름
-#             "quantity": "1",                # 구매 물품 수량
-#             "total_amount": "12000",        # 구매 물품 가격
-#             "tax_free_amount": "0",         # 구매 물품 비과세
-#             "approval_url": "paySuccess/",
-#             "cancel_url": "payCancel/",
-#             "fail_url": "payFail/",
-#         }
-
-#         res = requests.post(URL, headers=headers, params=params)
-#         request.session['tid'] = res.json()['tid']      # 결제 승인시 사용할 tid를 세션에 저장
-#         next_url = res.json()['next_redirect_pc_url']   # 결제 페이지로 넘어갈 url을 저장
-#         return redirect(next_url)
     
-   
+# @csrf_exempt
+# def canvasToImage(request):
+#     data = result.POST.__getitem__('data')
+#     data = data[22:]
+#     number = random.randrange(1,1000)
+
+#     path = str(os.path.join(settings.STATIC_ROOT, 'static/image/'))
+#     filename = 'image' + str(number) + '.png'
+
+#     image = open(path+filename,"wb")
+#     image.write(base64.b64decode(data))
+#     image.close()
+
+#     answer = {'filename':filename}
+#     return JsonResponse(answer)
+
 
 def payFail(request):
     return render(request, 'payFail.html')
