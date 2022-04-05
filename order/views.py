783a0e3a8188f0e8f23c7af57f3ae4fa653e13ed
jjangseo1 <93521234+seoheejj1@users.noreply.github.com>
2022-04-05 14:40:39 +0900

    sh#0405_3

--- order/views.py
+++ order/views.py
@@ -22,6 +22,7 @@ def order(request):
                 'session': request.session,
                 'config': Config.objects.get(id=1),
                 'currentpage': 'shopping',
+                'title':request.title,
             }
             return render(request, 'order.html', context)
         else:
@@ -59,25 +60,19 @@ def custom1(request):
     return render(request, 'custom1.html',context)
 
 def custom2(request):
+    color = request.GET.get('color_input')
+    size = request.GET.get('size_input')
     context = {
         'session': request.session,
         'config': Config.objects.get(id=1),
-        'currentpage': 'shopping'
+        'currentpage': 'shopping',
+        'color': color,
+        'size':size,
     }
-
     return render(request, 'custom2.html',context)
 
 
 def customend(request):
-
-    # if request.POST['t_input'] == "0":
-    #     return HttpResponse(f''' 
-    #         <script>
-    #             alert("커스텀을 하나 이상 적용시켜주세요!);
-    #             window.history.back();
-    #         </script>
-    #     ''')
-
     context = {
         'session': request.session,
         'config': Config.objects.get(id=1),
@@ -115,28 +110,6 @@ def payment(request):
         'currentpage': 'shopping'
     }
     context['user'] = User.objects.get(id=request.session['user'])
-    # if request.method == "POST":
-    #     URL = 'https://kapi.kakao.com/v1/payment/ready'
-    #     headers = {
-    #         'Authorization': "KaKaoAk" + "953b61f21c9f3ddc6d49f10981368940",
-    #         'Content-type': "application/x-www-form-urlencoded;charset=utf-8",
-    #     }
-    #     params = {
-    #         "cid": "TC0ONETIME",    # 테스트용 코드
-    #         "partner_order_id": "1001",     # 주문번호
-    #         "partner_user_id": "german",    # 유저 아이디
-    #         "item_name": "연어초밥",        # 구매 물품 이름
-    #         "quantity": "1",                # 구매 물품 수량
-    #         "total_amount": "12000",        # 구매 물품 가격
-    #         "tax_free_amount": "0",         # 구매 물품 비과세
-    #         "approval_url": "결제 성공 시 이동할 url",
-    #         "cancel_url": "결제 취소 시 이동할 url",
-    #         "fail_url": "결제 실패 시 이동할 url",
-    #     }
-    #     res = requests.post(URL, headers=headers, params=params)
-    #     request.session['tid'] = res.json()['tid']
-    #     next_url = res.json()['next_redirect_pc_url']
-    #     return redirect(next_url)
 
     return render(request,'payment.html',context)
 
